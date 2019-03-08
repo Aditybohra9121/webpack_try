b166070c3e4be114426c0d98e99afdd490759c14
Aditya Bohra <abohra@localhost.localdomain>
2019-03-09 00:12:19 +0530

    first time webpack

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import "../css/main.css";
+class coffee{
+constructor(){
+  console.log("fjk")
+}
+
+
+}
+let c=new coffee();
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath:"/"		  
+  },
+	module:{
+		rules:[
+			{
+				test: /\.css$/,
+				use:[
+					'style-loader',
+				           'css-loader',
+
+				]
+			}
+		]
+	}	
+};
